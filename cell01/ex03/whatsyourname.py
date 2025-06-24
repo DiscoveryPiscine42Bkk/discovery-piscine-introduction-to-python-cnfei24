9b8a24b620a600dc73d3ab60c7a495ee6fd9dfe2
cnfei24 <151550281+cnfei24@users.noreply.github.com>
2025-06-24 06:15:37 +0000

    what

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
 first= input("input your first name ")
 last = input("input your last name ")
-print("Well, pleased to meet you" + first +" "+ last)
+print("Well, pleased to meet you " + first +" "+ last)
---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import sys
+del sys.argv[0]
+numsy = len(sys.argv)
+print (numsy)
